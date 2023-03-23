51f31c9068450efcc502cc632ec9d5a00e4e6b22
Ken Davis <kd_112@protonmail.com>
2023-03-22 21:03:46 -0400

    longestCommonPrefix

--- longest_common_prefix/longest_common_prefix.py
+++ longest_common_prefix/longest_common_prefix.py
@@ -1,3 +1,20 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
         
+        shortest_string = min(strs, key=len)
+
+        common_prefix = ""
+
+        # Iterate through the characters of the shortest string
+        for i in range(len(shortest_string)):
+            current_char = shortest_string[i]
+
+        # Check if the current character is common across all strings
+            if all(string[i] == current_char for string in strs):
+                common_prefix += current_char
+            else:
+                break
+
+    return common_prefix
